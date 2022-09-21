28020fade269f13b8567b3d82152d25eb5f9d2e2
TheBluekr <kasper.ruhe@gmail.com>
2022-09-21 15:10:46 +0200

    Add embed colors to match user colors Fixed volume to not to go blast ears by default Added some code to enable loops down the line

--- cogs/lavalink.py
+++ cogs/lavalink.py
@@ -1,6 +1,6 @@
 import discord
 import asyncio
-
+import os
 import json
 
 from discord.ext import commands
@@ -33,18 +33,37 @@ class Lavalink(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-        self.bot.loop.create_task(self.connect_nodes())
+        self.bot.loop.create_task(self.post_init())
+
+        self.embedColors = {}
 
         self.wavequeue = {}
+        self.volume = {}
         self.sbClient = sponsorblock.Client()
 
         self.logger = logger
+
+    async def post_init(self):
+        await self.connect_nodes()
+        await self.embed_colors()
 
     async def connect_nodes(self):
         """Connect to our Lavalink nodes."""
         await self.bot.wait_until_ready()
 
         await wavelink.NodePool.create_node(bot=self.bot, host='lavalink', port=8080, password='youshallnotpass')
+    
+    async def embed_colors(self):
+        if not os.path.isfile(f"{os.getcwd()}/settings/music.embed.json"):
+            return
+        try:
+            with open(f"{os.getcwd()}/settings/music.embed.json", "r") as file:
+                embedColors = json.load(file)
+            for key in embedColors.keys():
+                self.embedColors[int(key)] = int(embedColors[key], 0)
+            self.logger.info(f"Loaded {len(self.embedColors)} embed colors for users")
+        except json.decoder.JSONDecodeError:
+            pass
     
     async def get_player(self, guild):
         node = await self.get_node()
@@ -63,51 +82,59 @@ class Lavalink(commands.Cog):
     @commands.Cog.listener()
     async def on_wavelink_track_start(self, player, track):
         curr = self.wavequeue[player.guild].get()
+        self.wavequeue[player.guild].put_at_front(curr)
         if(curr.identifier != track.identifier):
             await player.channel.send("Mismatch occured between expected and playing track, this is not intended.\nPlease contact the bot author if this occured.")
             await player.stop()
             return
         self.logger.info(f"Started playing: {curr.title} ({curr.identifier})")
+        await player.set_volume(self.volume[player.guild])
 
-        segments = self.sbClient.get_skip_segments(curr.identifier)
-        segments.sort(key=lambda x: x.start)
-
-        skips = self.process_sponsorblock(segments)
-
-        await player.set_volume(0.2)
-
-        # Prevent our player from lagging out the start
-        if(curr.sb):
-            await player.pause()
-            currPos = 0
-            if(len(skips) > 0):
-                if(currPos >= skips[0][0]):
-                    self.logger.info(f"Skipping segment {skips[0][0]}-{skips[0][1]} at start")
-                    await player.seek(skips[0][1]*1000)
-                    del skips[0]
-            await player.resume()
         embed = discord.Embed(description=f"**Started playing**:\n**[{track.title}](https://www.youtube.com/watch?v={track.identifier} '{track.identifier}')**")
+        if(curr.loop):
+            embed.description += " (looped)"
         embed.set_footer(text=f"Requested by: {curr.requester}", icon_url=curr.requester.avatar.url)
+        embed.color = self.embedColors.get(curr.requester.id, curr.requester.color)
         
         await player.channel.send(embed=embed)
 
-        if(curr.sb):
-            while(len(skips) > 0):
-                currPos = player.position
-                if(skips[0][0] < currPos < skips[0][1]):
-                    self.logger.info(f"Skipping segment {skips[0][0]}-{skips[0][1]}")
-                    await player.seek(skips[0][1]*1000)
-                    del skips[0]
-                elif(skips[0][1] < currPos):
-                    self.logger.info(f"Went past segment {skips[0][0]}-{skips[0][1]}, removing")
-                    del skips[0]
-                await asyncio.sleep(0.5)
+        try:
+            segments = self.sbClient.get_skip_segments(curr.identifier)
+            segments.sort(key=lambda x: x.start)
+
+            skips = self.process_sponsorblock(segments)
+
+            # Prevent our player from lagging out the start
+            if(curr.sb):
+                await player.pause()
+                currPos = 0
+                if(len(skips) > 0):
+                    if(currPos >= skips[0][0]):
+                        self.logger.info(f"Skipping segment {skips[0][0]}-{skips[0][1]} at start")
+                        await player.seek(skips[0][1]*1000)
+                        del skips[0]
+                await player.resume()
+                while(len(skips) > 0):
+                    currPos = player.position
+                    if(skips[0][0] < currPos < skips[0][1]):
+                        self.logger.info(f"Skipping segment {skips[0][0]}-{skips[0][1]}")
+                        await player.seek(skips[0][1]*1000)
+                        del skips[0]
+                    elif(skips[0][1] < currPos):
+                        self.logger.info(f"Went past segment {skips[0][0]}-{skips[0][1]}, removing")
+                        del skips[0]
+                    await asyncio.sleep(0.5)
+        except sponsorblock.errors.NotFoundException:
+            pass
 
     @commands.Cog.listener('on_wavelink_track_end')
     @commands.Cog.listener('on_wavelink_track_exception')
     @commands.Cog.listener('on_wavelink_track_stuck')
     async def on_player_stop(self, player: wavelink.Player, track, reason):
-        self.logger.info(f"Finished playing")
+        previoustrack = self.wavequeue[player.guild].get()
+        self.logger.info(f"Finished playing {previoustrack.title}")
+        if(previoustrack.loop):
+            self.wavequeue[player.guild].put_at_front(previoustrack)
         if(self.wavequeue[player.guild].count > 0):
             nexttrack = self.wavequeue[player.guild].get()
             self.wavequeue[player.guild].put_at_front(nexttrack)
@@ -116,7 +143,7 @@ class Lavalink(commands.Cog):
             await player.disconnect()
 
     @commands.command()
-    async def play(self, ctx: commands.Context, *, search: typing.Union[wavelink.YouTubeTrack, wavelink.YouTubeMusicTrack, wavelink.SoundCloudTrack]=None, loop: bool=False, sponsorblock: bool=True):
+    async def play(self, ctx: commands.Context, *, search: typing.Union[wavelink.YouTubeTrack, wavelink.YouTubeMusicTrack, wavelink.SoundCloudTrack]=None):
         """Play a song with the given search query.
 
         If not connected, connect to our voice channel.
@@ -125,13 +152,13 @@ class Lavalink(commands.Cog):
             vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
         else:
             vc: wavelink.Player = ctx.voice_client
-
         if(search != None):
-            track = Track(search.id, search.info, requester=ctx.author, sponsorblock=sponsorblock, loop=loop)
+            track = Track(search.id, search.info, requester=ctx.author, sponsorblock=True, loop=False)
             self.wavequeue[ctx.guild].put(track)
 
             embed = discord.Embed(description=f"**Added**:\n**[{track.title}](https://www.youtube.com/watch?v={track.identifier} '{track.identifier}')**")
             embed.set_footer(text=f"Requested by: {track.requester}", icon_url=track.requester.avatar.url)
+            embed.color = self.embedColors.get(track.requester.id, track.requester.color)
             await vc.channel.send(embed=embed)
             if(vc.is_paused()):
                 await vc.resume()
@@ -149,37 +176,45 @@ class Lavalink(commands.Cog):
     
     @commands.command()
     async def pause(self, ctx: commands.Context):
+        """Pauses the music player.
+        """
         if not ctx.voice_client:
             return
         vc: wavelink.Player = ctx.voice_client
         if(vc.source == None):
             return
         if(vc.is_playing()):
-            embed = discord.Embed(description=f"Paused **{vc.track.title}**")
+            embed = discord.Embed(description=f"Paused **[{vc.track.title}](https://www.youtube.com/watch?v={vc.track.identifier} '{vc.track.identifier}')**")
+            embed.color = self.embedColors.get(ctx.me.id, ctx.me.color)
             await ctx.channel.send(embed=embed)
             await vc.pause()
     
     @commands.command()
     async def resume(self, ctx: commands.Context):
+        """Unpauses the music player.
+        """
         if(not ctx.voice_client):
             return
         vc: wavelink.Player = ctx.voice_client
         if(vc.source == None):
             return
         if(vc.is_playing()):
-            embed = discord.Embed(description=f"Resumed **{vc.track.title}**")
+            embed = discord.Embed(description=f"Resumed **[{vc.track.title}](https://www.youtube.com/watch?v={vc.track.identifier} '{vc.track.identifier}')**")
+            embed.color = self.embedColors.get(ctx.me.id, ctx.me.color)
             await ctx.channel.send(embed=embed)
             await vc.resume()
     
     @commands.command()
     async def stop(self, ctx: commands.Context):
+        """Stops playing music and disconnects the bot from the voice channel."""
         if(not ctx.voice_client):
             return
         vc: wavelink.Player = ctx.voice_client
         if(vc.source == None):
             return
         if(vc.is_playing()):
-            embed = discord.Embed(description=f"Stopped playing")
+            embed = discord.Embed(description=f"Disconnecting")
+            embed.color = self.embedColors.get(ctx.me.id, ctx.me.color)
             await ctx.channel.send(embed=embed)
             self.wavequeue[ctx.guild].clear()
             await vc.stop()
@@ -187,24 +222,32 @@ class Lavalink(commands.Cog):
 
     @commands.command()
     async def skip(self, ctx: commands.Context):
+        """Skips the current playing song.
+        """
         if(not ctx.voice_client):
             return
         vc: wavelink.Player = ctx.voice_client
         if(vc.source == None):
             return
+        curr = self.wavequeue[vc.guild].get()
+        curr.loop = False
+        self.wavequeue[vc.guild].put_at_front(curr)
         await vc.stop()
     
     @commands.command()
-    async def volume(self, ctx: commands.Context, volume: float=100.0):
+    async def volume(self, ctx: commands.Context, volume: float=1.0):
+        """Set the volume for this guild. (Default 1.0)"""
         if(not ctx.voice_client):
             return
         vc: wavelink.Player = ctx.voice_client
         if(vc.source == None):
             return
+        vc.track.loop = False
         await vc.set_volume(volume)
     
     @commands.command()
     async def seek(self, ctx: commands.Context, position: float):
+        """Sets the current playback to the specified time (in seconds)"""
         if(not ctx.voice_client):
             return
         vc: wavelink.Player = ctx.voice_client
@@ -215,6 +258,7 @@ class Lavalink(commands.Cog):
     @play.before_invoke
     async def ensure_queue(self, ctx: commands.Context):
         self.wavequeue[ctx.guild] = wavelink.Queue()
+        self.volume[ctx.guild] = 1.0
     
     def process_sponsorblock(self, sponsorblock_segments: list):
         blocks = []
