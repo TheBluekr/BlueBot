ff87782eef1a990e26d18450200ac094c8984f09
TheBluekr <kasper.ruhe@gmail.com>
2023-05-31 15:36:27 +0200

    Add more code to feedback when updating files

--- core/git.py
+++ core/git.py
@@ -18,24 +18,32 @@ class Git:
         self.repo = git.Repo(".")
         self.logger.info("Initialized Git repo")
 
+        self.updateChannel = os.getenv("GIT_CHANNEL")
+
     @tasks.loop(seconds=60)
     async def update_code(self):
         origin = self.repo.remotes.origin
         origin.fetch()
         diff = self.repo.index.diff(origin.refs['Development'].object.hexsha)
-        origin.pull()
-        for file in diff:
-            apath = file.a_path
-            apath = apath.replace("/", ".").replace(".py", "")
-            if(apath == "bot" or apath.startswith("core.")):
-                # Reboot entire bot to load new bot.py or core/file.py
-                exit(1)
-            elif(apath.startswith("cogs.")):
-                if(diff.change_type in ["D", "R", "M", "T"]):
-                    await self.bot.unload_extension(apath)
-            
-            bpath = file.b_path
-            bpath = bpath.replace("/", ".").replace(".py", "")
-            if(bpath.startswith("cogs.")):
-                if(diff.change_type in ["A", "R", "M"]):
-                    await self.bot.load_extension(bpath)
+        if(len(diff) > 0):
+            self.logger.info("File update found on repo, updating files")
+            channel = discord.utils.get(self.bot.get_all_channels(), id=self.updateChannel)
+            embed = self.bot.embed.create_embed(self.bot.user)
+            embed.description = "Update found on repository, updating code"
+            await channel.send(embed=embed)
+            origin.pull()
+            for file in diff:
+                apath = file.a_path
+                apath = apath.replace("/", ".").replace(".py", "")
+                if(apath == "bot" or apath.startswith("core.")):
+                    # Reboot entire bot to load new bot.py or core/file.py
+                    exit(1)
+                elif(apath.startswith("cogs.")):
+                    if(diff.change_type in ["D", "R", "M", "T"]):
+                        await self.bot.unload_extension(apath)
+                
+                bpath = file.b_path
+                bpath = bpath.replace("/", ".").replace(".py", "")
+                if(bpath.startswith("cogs.")):
+                    if(diff.change_type in ["A", "R", "M"]):
+                        await self.bot.load_extension(bpath)
