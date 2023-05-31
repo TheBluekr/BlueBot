5b89293c0345f0d350f5ab83cd8c96999a5a339f
TheBluekr <kasper.ruhe@gmail.com>
2023-05-31 14:23:43 +0200

    Remove lavalink due to being broken Add git

--- core/git.py
+++ core/git.py
@@ -1,6 +1,7 @@
-import git
 import discord
 from discord.ext import tasks
+import git
+import os
 import logging
 
 __author__ = "TheBluekr#2702"
@@ -13,7 +14,32 @@ class Git:
 
         self.bot = bot
         self.db = bot.db
+
+        self.repo = git.Repo(".")
+        self.logger.info("Initialized Git repo")
+
+        # self.update_code.start()
+        self.logger.info(f"{self.repo.head.object.hexsha}")
+        origin = self.repo.remotes.origin
+        origin.fetch()
+        self.logger.info(f"{origin.refs['Development'].object.hexsha}")
+        diff = self.repo.index.diff(origin.refs['Development'].object.hexsha)
+        origin.pull()
+        self.logger.info(f"{diff}")
+        for file in diff:
+            path = file.a_path
+            path = path.replace("/", ".").replace(".py", "")
+            if(path == "bot" or path.startswith("core.")):
+                # Reboot entire bot to load new bot.py or core/file.py
+                pass
+            elif(path.startswith("cogs.")):
+                self.bot.unload_extension(path)
+                self.bot.load_extension(path)
+            self.logger.info(path)
     
     @tasks.loop(seconds=60)
     async def update_code(self):
-        pass
+        self.logger.info(f"{self.repo.head.object.hexsha}")
+        origin = self.repo.remotes.origin
+        origin.fetch()
+        self.logger.info(f"{origin.refs['Development'].object.hexsha}")
