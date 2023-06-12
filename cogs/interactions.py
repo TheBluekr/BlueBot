c8b6d8720492fb84e4be3d1b65ea4290a89f8ba3
Cassandra Ruhe <16746102+TheBluekr@users.noreply.github.com>
2023-06-12 10:50:46 +0200

    Update more cogs to slash commands
    Update more cogs to slash commands
    Fix errors on eval_fn

--- cogs/interactions.py
+++ cogs/interactions.py
@@ -1,6 +1,7 @@
 import logging
 import discord
 import typing
+import re
 
 from discord import app_commands
 from discord.ext import commands
@@ -14,6 +15,8 @@ class Buttons(commands.Cog):
         self.bot = bot
         self.logger = logger
         self.logger.info(f"Loaded cog {__cogname__}")
+        self.embed = self.bot.embed
+        
         self.cache = {}
     
     def is_guild_owner():
@@ -39,61 +42,93 @@ class Buttons(commands.Cog):
                     await channel.set_permissions(interaction.user, overwrite=perms)
                 await interaction.response.defer()
 
-    @is_guild_owner()
-    @commands.guild_only()
-    @commands.group(pass_context=True)
-    async def button(self, ctx):
-        pass
-
-    @button.group(pass_context=True)
-    async def add(self, ctx):
-        pass
-
-    @add.command()
-    async def role(self, ctx, message: discord.Message, label: str, style: str, role: discord.Role, emoji: typing.Union[discord.PartialEmoji, discord.Emoji, str]=None):
-        view = discord.ui.View.from_message(message)
-        style = getattr(discord.ButtonStyle, style)
-        view.add_item(discord.ui.Button(label=label, style=style, emoji=emoji, custom_id=f"role.{role.id}"))
-        await message.edit(view=view)
-    
-    @add.command()
-    async def channel(self, ctx, message: discord.Message, label: str, style: str, channel: typing.Union[discord.TextChannel, discord.VoiceChannel], emoji: typing.Union[discord.PartialEmoji, discord.Emoji, str]=None):
-        view = discord.ui.View.from_message(message)
-        style = getattr(discord.ButtonStyle, style)
-        view.add_item(discord.ui.Button(label=label, style=style, emoji=emoji, custom_id=f"channel.{channel.id}"))
-        await message.edit(view=view)
-    
-    @add.error
-    async def add_error(self, ctx, error):
-        if(isinstance(error, commands.errors.MissingRequiredArgument)):
-            await ctx.send(f"Missing required argument, please check whether you've provided all required arguments")
-        elif(isinstance(error, commands.errors.RoleNotFound)):
-            await ctx.send(f"Could not find role with provided id")
-        elif(isinstance(error, commands.errors.MessageNotFound)):
-            await ctx.send(f"Could not find message with provided id")
+    button = app_commands.Group(name="button", description="Manage buttons on messages")
     
     @button.command()
-    async def url(self, ctx, message: discord.Message, label: str, url: str, emoji: typing.Union[discord.PartialEmoji, discord.Emoji, str]=None):
+    @app_commands.checks.has_permissions(manage_guild=True)
+    @app_commands.choices(style=[app_commands.Choice(name="primary", value="primary"),
+                                app_commands.Choice(name="secondary", value="secondary"),
+                                app_commands.Choice(name="success", value="success"),
+                                app_commands.Choice(name="danger", value="danger"),
+                                app_commands.Choice(name="link", value="link"),
+                                app_commands.Choice(name="blurple", value="primary"),
+                                app_commands.Choice(name="grey", value="secondary"),
+                                app_commands.Choice(name="gray", value="secondary"),
+                                app_commands.Choice(name="green", value="success"),
+                                app_commands.Choice(name="red", value="danger"),
+                                app_commands.Choice(name="url", value="link")])
+    @app_commands.choices(type=[app_commands.Choice(name="Role", value="role"),
+                                app_commands.Choice(name="Channel", value="channel"),
+                                app_commands.Choice(name="URL", value="url")])
+    async def add(self, interaction: discord.Interaction, message: str, label: str, style: app_commands.Choice[str], type: app_commands.Choice[str], value: str, emoji: str=None):
+        message = await interaction.channel.fetch_message(int(message))
         view = discord.ui.View.from_message(message)
-        view.add_item(discord.ui.Button(label=label, style=discord.ButtonStyle.url, url=url))
+        style = getattr(discord.ButtonStyle, style.value)
+
+        embed = self.embed.create_embed(interaction.user)
+
+        if(message == None):
+            embed.description = "```Failed to find message```"
+            return await interaction.response.send_message(embed=embed)
+
+        if(type.value == "role"):
+            match = re.compile(r'([0-9]{15,20})$').match(value) or re.match(r'<@&([0-9]{15,20})>$', value)
+            if match:
+                role = interaction.guild.get_role(int(match.group(1)))
+            else:
+                role = discord.utils.get(interaction.guild.roles, name=value)
+            if role is None:
+                return await interaction.response.send_message("No role found")
+            view.add_item(discord.ui.Button(label=label, style=style, emoji=emoji, custom_id=f"role.{role.id}"))
+        elif(type.value == "channel"):
+            match = re.compile(r'([0-9]{15,20})$').match(value) or re.match(r'<#([0-9]{15,20})>$', value)
+            channel = None
+            guild = interaction.guild
+
+            if(match == None):
+                channel = discord.utils.get(guild.channels, name=value)
+            else:
+                channel_id = int(match.group(1))
+                channel = guild.get_channel(channel_id)
+
+            if(channel == None):
+                embed.description = "```Failed to find channel```"
+                return await interaction.response.send_message(embed=embed)
+            view.add_item(discord.ui.Button(label=label, style=style, emoji=emoji, custom_id=f"channel.{channel.id}"))
+        elif(type.value == "url"):
+            view.add_item(discord.ui.Button(label=label, style=discord.ButtonStyle.url, url=value))
+        
         await message.edit(view=view)
+        embed.description = "```Added button```"
+        await interaction.response.send_message(embed=embed)
     
     @button.command()
-    async def clear(self, ctx, message: discord.Message):
+    @app_commands.checks.has_permissions(manage_guild=True)
+    async def clear(self, interaction: discord.Interaction, message: int):
+        message = await interaction.channel.fetch_message(message)
         await message.edit(view=None)
+        embed = self.embed.create_embed(interaction.user)
+        embed.description = "```Cleared buttons```"
+        await interaction.response.send_message(embed=embed)
 
     @commands.is_owner()
     @commands.guild_only()
     @commands.command()
-    async def sync(self, ctx):
+    async def sync(self, ctx: commands.Context):
         fmt = await self.bot.tree.sync(guild=ctx.guild)
         await ctx.send(f"Synced {len(fmt)} commands for guild")
     
     @commands.is_owner()
     @commands.command()
-    async def globalsync(self, ctx):
-        fmt = await self.bot.tree.sync()
+    async def globalsync(self, ctx: commands.Context):
+        fmt = await self.bot.tree.sync(guild=None)
         await ctx.send(f"Synced {len(fmt)} commands globally")
+    
+    @commands.is_owner()
+    @commands.command()
+    async def clearsync(self, ctx: commands.Context):
+        ctx.bot.tree.clear_commands(guild=None)
+        await ctx.bot.tree.sync(guild=None)
     
     @commands.hybrid_command(description="Returns latency of bot")
     async def ping(self, ctx):
