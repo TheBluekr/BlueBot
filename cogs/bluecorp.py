ae29a47a58a34ad6f98f1db2ee67e6cde97cff82
TheBluekr <kasper.ruhe@gmail.com>
2023-06-21 14:22:56 +0200

    Add missing response to app_command

--- cogs/bluecorp.py
+++ cogs/bluecorp.py
@@ -102,8 +102,9 @@ class Bluecorp(commands.Cog):
     @app_commands.command()
     @app_commands.checks.has_permissions(manage_messages=True)
     @app_commands.guild_only()
-    async def purge(self, ctx, limit: int=100):
-        await ctx.channel.purge(limit=limit)
+    async def purge(self, interaction: discord.Interaction, limit: int=100):
+        await interaction.channel.purge(limit=limit)
+        await interaction.response.send_message(f"Purged {limit} messages", ephemeral=True)
 
     @app_commands.command()
     @app_commands.guilds(discord.Object(138365437791567872))
